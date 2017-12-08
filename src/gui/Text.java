23f669655b6de06a64e471d580d1feefae672869
ntxtung <xuantung.1998@gmail.com>
2017-12-08 12:41:54 +0700

    Improve Text

--- src/gui/Text.java
+++ src/gui/Text.java
@@ -14,29 +14,47 @@ import org.newdawn.slick.TrueTypeFont;
 import org.newdawn.slick.util.ResourceLoader;
 
 public class Text {
-	private boolean antiAlias = true;
+	private static String fontDir = "res/Fonts/FbUsv8C5eI.ttf";
 	
-	private Font awtFont = new Font("Times New Roman", Font.BOLD, 24);
-	private TrueTypeFont font = new TrueTypeFont(awtFont, antiAlias);
-
-    public void loadFont(String fontDirection, float fontSize) {
-        try {
-            InputStream inputStream = ResourceLoader.getResourceAsStream(fontDirection);
+	private static Text instance = null;
+	private static boolean antiAlias = true;
+	
+	private static Font awtFont = new Font("Times New Roman", Font.BOLD, 24);
+	private static TrueTypeFont font35 = new TrueTypeFont(awtFont, antiAlias);
+	private static TrueTypeFont font70 = new TrueTypeFont(awtFont, antiAlias);
+	private static InputStream inputStream;
+	
+	private static void loadFont() {
+		try {
+            inputStream = ResourceLoader.getResourceAsStream(fontDir);
              
             Font awtFont = Font.createFont(Font.TRUETYPE_FONT, inputStream);
-            awtFont = awtFont.deriveFont(fontSize); // set font size
-            font = new TrueTypeFont(awtFont, antiAlias);
+            font35 = new TrueTypeFont(awtFont.deriveFont(35.0f * PZGUI.resolutionRateHeight), antiAlias);
+            font70 = new TrueTypeFont(awtFont.deriveFont(70.0f * PZGUI.resolutionRateHeight), antiAlias);
              
         } catch (Exception e) {
             e.printStackTrace();
         }
+	}
+	
+    
+    public Text() {
+    	loadFont();
+    }
+
+    public static void render35(float x, float y, String whatchars) {
+        font35.drawString(x, y, whatchars);
     }
     
-    public void render(float x, float y, String whatchars) {
-        font.drawString(x, y, whatchars);
+    public static void render35(float x, float y, String whatchars, Color color){
+    	font35.drawString(x, y, whatchars, color);
     }
     
-    public void render(float x, float y, String whatchars, Color color){
-    	font.drawString(x, y, whatchars, color);
+    public static void render70(float x, float y, String whatchars) {
+        font70.drawString(x, y, whatchars);
+    }
+    
+    public static void render70(float x, float y, String whatchars, Color color){
+    	font70.drawString(x, y, whatchars, color);
     }
 }
