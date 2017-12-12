88e6720f7572540f3869766924c328f4d85a8863
Nguyễn Phan Hùng Thuận <hungthuanmk@gmail.com>
2017-12-12 20:42:10 +0700

    Add attack Animation

--- src/pz/zombie/FemaleZombie.java
+++ src/pz/zombie/FemaleZombie.java
@@ -2,6 +2,8 @@ package pz.zombie;
 
 import java.util.ArrayList;
 
+import org.newdawn.slick.Animation;
+
 import com.Position;
 
 import gui.AnimationLoader;
@@ -15,10 +17,15 @@ public class FemaleZombie extends Zombie {
 	private static int damage = 100;
 	private static int attackInterval = 50;
 	private static float speed = 0.4f;
+	
+	private Animation walkAni;
+	private Animation attackAni;
 	//private static float scaleFactor = 0.2f;
 
 	public FemaleZombie(Position pos) {
 		super("FemaleZombie", hp, damage, attackInterval, speed, pos);
+		walkAni = getAnimation();
+		attackAni = AnimationLoader.getAnimationFromFolder("res/ZombieTest/FemaleZombie/attack", 110);
 	}
 
 	@Override
@@ -40,6 +47,7 @@ public class FemaleZombie extends Zombie {
 					if (Position.isInteract(this, plant[i][j])) {
 						hit = true;
 						setSpeed(0);
+						setAnimation(attackAni);
 						//System.out.println("touched");
 						if (getFramePassed() >= getAttackInterval()) {
 							plant[i][j].setHp(plant[i][j].getHp() - getDamage());
@@ -51,7 +59,10 @@ public class FemaleZombie extends Zombie {
 				}			
 			}
 		}
-		if (hit == false) setSpeed(speed);
+		if (hit == false) {
+			setAnimation(walkAni);
+			setSpeed(speed);
+		}
 	}
 
 }
