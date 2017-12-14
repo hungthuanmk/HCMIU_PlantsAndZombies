c813a9e3cf24f37ab9b5ec6d50abb8e18bc0e3c7
ntxtung <xuantung.1998@gmail.com>
2017-12-14 12:13:35 +0700

    Add

--- src/gui/PZGUI.java
+++ src/gui/PZGUI.java
@@ -67,7 +67,7 @@ public class PZGUI extends StateBasedGame {
 		this.addState(new SplashScreen(splashScreen));
 		this.addState(new Menu(menu));
 		this.addState(new Play(play));
-		//this.addState(new GameOver(gameOver));
+		this.addState(new GameOver(gameOver));
 	}
 	
 	/**
